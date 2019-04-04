1faf9796d8a20c414ddc9c69b880756476126f8d
Vladimir101 <vladimir100@hotmail.com>
2019-04-03 18:00:59 -0700

    added more comments

--- 3. JSONParser/src/test/java/com/rest/ParseJSON.java
+++ 3. JSONParser/src/test/java/com/rest/ParseJSON.java
@@ -1,4 +1,5 @@
 package com.rest;
+// Parser is org.json (JSON In Java)
 /*JSON Response
 {
     "hey": "guy",
@@ -41,21 +42,32 @@ public class ParseJSON
 	public void parseGet() throws IOException
 	{
 		String page = restTemplate.getForObject(URL, String.class);
+// Since response contains Japanese characters, it will be encoded in
+// in Unicode. The first character will be byte order mark 65279, and not {
+// https://en.wikipedia.org/wiki/Byte_order_mark
+
+// int first = page.charAt(0);
+// System.out.println(first);		
+// We'll get 65279, as expected
 		
-// Parser validates the JSON response.
-// It will process it as a JSONObject if it starts with '{' as the first character
+// Parser will process JSON response as a JSONObject only if it starts with '{' 
+// in the first position
 		int pos = page.indexOf("{");
 		page = page.substring(pos);
-		
+
 		JSONObject response = new JSONObject(page);
 		System.out.println(response.getString("hey"));
-		
+
+		int anumber = response.getInt("anumber");
+		System.out.println(anumber);
+
 		JSONObject anobject = response.getJSONObject("anobject");
-		JSONArray jsonArray = anobject.getJSONArray("anarray");
-		int element1 = (Integer) jsonArray.get(1);
+		JSONArray anarray = anobject.getJSONArray("anarray");
+		
+		int element1 = anarray.getInt(1);
 		System.out.println("Element 1: " + element1);
-		  
-		String element2 = jsonArray.get(2).toString();
+		
+		String element2 = anarray.getString(2);
 		System.out.println("Element 2: " + element2);
 	}
 }
